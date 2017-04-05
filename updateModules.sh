4d307c3c4216d3f2de929bed8123dad0dfe2d487
ellisa4 <ellisa4@xavier.edu>
2017-04-05 18:41:46 -0400

    project | Ellis | removed updateModules.sh

--- updateModules.sh
+++
@@ -1,8 +0,0 @@
-git config credential.helper store
-cd src/main/resources
-git add *
-echo "Give a commit message:"
-read commitMessage
-"git commit -m $commitMessage"
-"git push --force origin master"
-cd ../../..
