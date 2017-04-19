3c24191a2461cd73b61eb8595191710e38fa6435
Luke McNamee <lucas.mcnamee@gmail.com>
2017-04-19 11:50:05 -0400

    Luke | 4/19 | Sorry I forgot to push this. You need this for your services to be able to log in.

--- schema.sql
+++ schema.sql
@@ -1,25 +1,21 @@
 DROP TABLE users IF EXISTS;
 CREATE TABLE users(
     id INTEGER AUTO_INCREMENT Primary Key,
-    username VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    password VARCHAR(255) NOT NULL,
-    enabled TINYINT NOT NULL DEFAULT 1
-    );
-
-DROP TABLE authorities IF EXISTS;
-CREATE TABLE authorities (
-    authority VARCHAR(255),
-    username VARCHAR(255)
+    username VARCHAR(60) NOT NULL,
+    first_name VARCHAR(60),
+    last_name VARCHAR(60),
+    password VARCHAR(60) NOT NULL,
+    enabled TINYINT NOT NULL DEFAULT 1,
+    manager_id VARCHAR(60),
+    role VARCHAR(15) NOT NULL
     );
 
 DROP TABLE messages IF EXISTS;
 CREATE TABLE messages(
     id INTEGER AUTO_INCREMENT Primary Key,
-    to_id VARCHAR(255),
-    from_id VARCHAR(255),
-    body VARCHAR(1000),
+    to_id VARCHAR(60),
+    from_id VARCHAR(60),
+    body VARCHAR(1023),
     subject VARCHAR(255),
     created DATETIME DEFAULT CURRENT_TIMESTAMP
     );
